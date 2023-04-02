483a4b5f44f4deeffc2f3c5094848fc725f9294a
lalitgawde <lalitgawde0@gmail.com>
2023-04-02 14:49:43 +0530

    Example1 Complete

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -2,17 +2,30 @@ import React from "react";
 import style from "./DropDown.module.css";
 
 const DropDown = (props) => {
+  console.log("props", props);
+  const onSelectHandler = (event) => {
+    if (props.label === "Country") {
+      props.onCountryChange(event, "Country");
+    }
+    if (props.label === "State") {
+      props.onStateChange(event, "State");
+    }
+    if (props.label === "City") {
+      props.onCityChange(event, "City");
+    }
+  };
   return (
     <>
       <div className={style.label}>
         <label>{props.label}</label>
       </div>
       <div className={style["dropDown-list"]}>
-        <select>
+        <select onChange={onSelectHandler} value={props.selectedItem}>
           <option style={{ fontSize: "15px" }}>Select {props.label}</option>
-          {props.options.map((option) => {
-            return <option>{option.value}</option>;
-          })}
+          {props.options &&
+            props.options.map((option) => {
+              return <option>{option.value}</option>;
+            })}
         </select>
       </div>
     </>
